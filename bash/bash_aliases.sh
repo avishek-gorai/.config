2e9297754da4480812d84eb5c4311b77c13f1090
Avishek Gorai <avishekgorai@myyahoo.com>
2025-06-14 12:16:16 +0000

    Added license and readme.

--- bash/bash_aliases.sh
+++ bash/bash_aliases.sh
@@ -2,7 +2,9 @@ alias ls='ls --ignore-backups -l'                      \
       rm='rm --interactive=once --recursive --verbose' \
       cp='cp --interactive --recursive --verbose'      \
       mv='mv --interactive --verbose'                  \
-      rmdir='rmdir --verbose'
+      rmdir='rmdir --verbose'                          \
+      sbcl='sbcl --userinit ~/.config/sbcl/init.lisp'
+
 
 
 # Local Variables:
