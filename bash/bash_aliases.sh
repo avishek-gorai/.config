0f739000ef3f7ed89fbb1b0fc3083a56bc00c6f0
Avishek Gorai <avishekgorai@myyahoo.com>
2025-06-16 13:18:39 +0000

    Added readline wrap to sbcl.

--- bash/bash_aliases.sh
+++ bash/bash_aliases.sh
@@ -1,9 +1,9 @@
-alias ls='ls --ignore-backups -l'                      \
-      rm='rm --interactive=once --recursive --verbose' \
-      cp='cp --interactive --recursive --verbose'      \
-      mv='mv --interactive --verbose'                  \
-      rmdir='rmdir --verbose'                          \
-      sbcl='sbcl --userinit ~/.config/sbcl/init.lisp'
+alias ls='ls --ignore-backups -l'                             \
+      rm='rm --interactive=once --recursive --verbose'        \
+      cp='cp --interactive --recursive --verbose'             \
+      mv='mv --interactive --verbose'                         \
+      rmdir='rmdir --verbose'                                 \
+      sbcl='rlwrap sbcl --userinit ~/.config/sbcl/init.lisp'
 
 
 
