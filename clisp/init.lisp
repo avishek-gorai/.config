300c29b53252f297f4c67da3290150b06e379ec8
Avishek Gorai <avishekgorai@myyahoo.com>
2025-06-17 11:02:22 +0000

    Added clisp configuration directory.

---
+++ clisp/init.lisp
@@ -0,0 +1,39 @@
+;;; Copyright (C) 2025 by Avishek Gorai
+;;;
+;;; This program is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+(proclaim (quote
+           (optimize
+            (compilation-speed 0)
+            (safety 3)
+            (debug 3)
+            (speed 0)
+            (space 0))))
+
+
+(defmacro ppmx (form)
+  "Pretty prints macro expansions of FORM."
+  `(let* ((exp1 (macroexpand-1 ',form))
+          (exp (macroexpand exp1))
+          (*print-circle* nil))
+     (cond ((equal exp exp1)
+            (format t "~&Macro expansion:")
+            (pprint exp))
+           (t (format t "~&First step of expansion:")
+              (pprint exp1)
+              (format t "~%~%Final expansion:")
+              (pprint exp)))
+     (format t "~%~%")
+     (values)))
