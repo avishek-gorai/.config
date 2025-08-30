45312f5554f98e9ba8ececb1b8c017db330f61ca
Avishek Gorai <avishekgorai@myyahoo.com>
2025-08-30 22:15:31 +0530

    Removed all shell aliases.  Maybe I am becoming a professional.

--- bash/bash_aliases.sh
+++ bash/bash_aliases.sh
@@ -1,11 +1,1 @@
-alias ls='ls --ignore-backups -l'                             \
-      rm='rm --interactive=once --recursive --verbose'        \
-      cp='cp --interactive --recursive --verbose'             \
-      mv='mv --interactive --verbose'                         \
-      rmdir='rmdir --verbose'                                 \
-
-
-
-# Local Variables:
-# mode: sh
-# End:
+alias ls='ls --ignore-backups'
